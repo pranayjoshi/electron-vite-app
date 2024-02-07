fbfb97f1f4b7b625ac97e5d41caa8fd058b90b0c
pranayjoshi <pranayjoshi4466@gmail.com>
2024-02-07 14:46:08 -0600

    setup and dry run successful

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
 import react from '@vitejs/plugin-react'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tsConfigPaths(),
     electron({
       main: {
         // Shortcut of `build.lib.entry`.
